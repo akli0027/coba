18a5576440d0a5b07f96751df6843b7bfa24f128
akli0027 <73159374+akli0027@users.noreply.github.com>
2021-06-30 23:02:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
 // Mengetahui IP Pengunjung
 function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
-    else
-        $ipaddress = 'IP tidak dikenali';
-    return $ipaddress;
+    $ip_user=$_SERVER['REMOTE_ADDR'];
 }
    echo "IP anda adalah : ". get_client_ip()."<br>";
 ?>
